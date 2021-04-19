f5b6cf59a8c92bdcdfdcd0f678fb7b31314e8778
Paulo Vitor Fernandes Andrade <69232148+PauloFAndrade@users.noreply.github.com>
2021-04-19 15:30:36 -0300

    Theorem plus_n_Sm

--- Induction.v
+++ Induction.v
@@ -215,7 +215,14 @@ Qed.
 Theorem plus_n_Sm : forall n m : nat,
   S (n + m) = n + (S m).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m.
+  induction n.
+    -simpl.
+     reflexivity.
+    -simpl.
+     rewrite IHn.
+     reflexivity.
+Qed.
 
 Theorem plus_comm : forall n m : nat,
   n + m = m + n.
