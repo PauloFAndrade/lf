160cffa29f0c7e7b2d7f48c61267e8492eed370b
Paulo Vitor Fernandes Andrade <69232148+PauloFAndrade@users.noreply.github.com>
2021-04-19 15:28:24 -0300

    Theorem mult_0_r

--- Induction.v
+++ Induction.v
@@ -204,7 +204,13 @@ Proof.
 Theorem mult_0_r : forall n:nat,
   n * 0 = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intro n.
+  induction n.
+    -reflexivity.
+    -simpl.
+     rewrite IHn.
+     reflexivity.
+Qed.
 
 Theorem plus_n_Sm : forall n m : nat,
   S (n + m) = n + (S m).
