338b79aca3dd42d207976f9d3b2f02bee9b8fdd2
Paulo Vitor Fernandes Andrade <69232148+PauloFAndrade@users.noreply.github.com>
2021-04-28 01:44:43 -0300

    Fixpoint nonzeros

--- Lists.v
+++ Lists.v
@@ -299,8 +299,12 @@ Proof. reflexivity. Qed.
     [countoddmembers] below. Have a look at the tests to understand
     what these functions should do. *)
 
-Fixpoint nonzeros (l:natlist) : natlist
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Fixpoint nonzeros (l:natlist) : natlist :=
+match l with
+| nil => nil
+| 0 :: h => nonzeros h
+| h :: t => h :: (nonzeros t)
+end.
 
 Example test_nonzeros:
   nonzeros [0;1;0;2;3;0;0] = [1;2;3].
