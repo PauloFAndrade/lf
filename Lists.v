9b6dab6a28ac6e4c2a37eb28c75e6928065f78f5
Paulo Vitor Fernandes Andrade <69232148+PauloFAndrade@users.noreply.github.com>
2021-04-28 01:58:09 -0300

    Example test_countoddmembers1:

--- Lists.v
+++ Lists.v
@@ -338,7 +338,10 @@ end.
 
 Example test_countoddmembers1:
   countoddmembers [1;0;3;1;4;5] = 4.
-  (* FILL IN HERE *) Admitted.
+Proof.
+  simpl.
+  reflexivity.
+Qed.
 
 Example test_countoddmembers2:
   countoddmembers [0;2;4] = 0.
