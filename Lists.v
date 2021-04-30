1afbc23f5ce4b060891d3b1bca80eec91833c896
Paulo Vitor Fernandes Andrade <69232148+PauloFAndrade@users.noreply.github.com>
2021-04-30 18:21:35 -0300

    Lemma nonzeros_app

--- Lists.v
+++ Lists.v
@@ -1022,7 +1022,18 @@ Qed.
 Lemma nonzeros_app : forall l1 l2 : natlist,
   nonzeros (l1 ++ l2) = (nonzeros l1) ++ (nonzeros l2).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros l1 l2.
+  induction l1.
+    -simpl.
+     reflexivity.
+    -destruct n.
+     +simpl.
+      rewrite IHl1.
+      reflexivity.
+     +simpl.
+      rewrite IHl1.
+      reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard (eqblist) 
